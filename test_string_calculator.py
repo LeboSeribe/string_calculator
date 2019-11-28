f806be77771b6e32234efa718f194c697b5448a9
olebogeng <seribeolebogeng@gmail.com>
2019-11-28 10:18:58 +0200

    test changes

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,4 +1,4 @@
-tuimport pytest
+import pytest
 from string_calculator import add
 #testing the add function
 import string_calculator
@@ -17,9 +17,16 @@ def test_function_of_delimeters():
 def test_function_of_negatives():
     with pytest.raises(ValueError) as VE:
         add('-5,-4')
-
     assert "negatives" in str(VE.value)
 def test_function_of_number_greater_than_1000():
     assert add('2,1001')
+
+def test_function_of_lengthy_delimeters():
+    assert add('//[***]\n1***2***3') == 6  
+
+def test_function_of_multiple_delimeters():
+    assert add('//[*][%]\n1*2%3') == 6   
+
+     
 
 
